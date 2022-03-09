61fc75a5fff0c7fff623d23d5e1a874e3a9953a8
Benjamin Levy <levybe@ssfs.org>
2022-03-09 14:24:21 -0500

    Ran into a problem when getting the title of a button that is pressed. The value comes back as "nil" instead of the actual letter, like "Q" or "T".

--- Apple Pie/Apple Pie/ViewController.swift
+++ Apple Pie/Apple Pie/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var treeImageView: UIView!
+    @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
@@ -28,18 +28,39 @@ class ViewController: UIViewController {
     
     func newRound() {
         let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
         updateUI()
     }
     
     func updateUI() {
-        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        var letters = [String]()
+            for letter in currentGame.formattedWord {
+                letters.append(String(letter))
+            }
+            let wordWithSpacing = letters.joined(separator: " ")
+            correctWordLabel.text = wordWithSpacing
+            scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
             treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
     
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
-    }
+        print(sender.title(for: .normal))
+//            let letterString = sender.title(for: .normal)!
+//            let letter = Character(letterString.lowercased())
+//            currentGame.playerGuessed(letter: letter)
+            updateGameState()
+        }
+
+        func updateGameState() {
+            if currentGame.incorrectMovesRemaining == 0 {
+                totalLosses += 1
+              } else if currentGame.word == currentGame.formattedWord {
+                totalWins += 1
+              } else {
+                updateUI()
+              }
+        }
     
     
 
