b03b9a3387bad870707a46ec8e891b6f1da76d92
Benjamin Levy <levybe@ssfs.org>
2022-04-05 11:01:20 -0400

    Coding is peculiar.

--- 3 - Navigation and Workflows/2 - Type Casting/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/2 - Type Casting/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
@@ -3,17 +3,37 @@
 
  Create a collection of type [Any], including a few doubles, integers, strings, and booleans within the collection. Print the contents of the collection.
  */
- 
-
+var collection: [Any] = [1.0, 9.1106, 17, 36, "Something", "Something else", true, false]
+print(collection)
 //:  Loop through the collection. For each integer, print "The integer has a value of ", followed by the integer value. Repeat the steps for doubles, strings and booleans.
- 
-
+for thing in collection {
+    if thing is Double {
+        print("The double has a value of \(thing).")
+    }
+    else if thing is Int {
+        print("The integer has a value of \(thing).")
+    }
+    else if thing is String {
+        print("The string has a value of \(thing).")
+    }
+    else if thing is Bool {
+        print("The boolean has a value of \(thing).")
+    }
+}
 //:  Create a [String : Any] dictionary, where the values are a mixture of doubles, integers, strings, and booleans. Print the key/value pairs within the collection
- 
-
+var things: [String: Any] = ["Double" : 8.77249, "Integer" : 9001, "String" : "Something else else?", "Boolean" : false]
+print (things)
 //:  Create a variable `total` of type `Double` set to 0. Then loop through the dictionary, and add the value of each integer and double to your variable's value. For each string value, add 1 to the total. For each boolean, add 2 to the total if the boolean is `true`, or subtract 3 if it's `false`. Print the value of `total`.
- 
+var total: Double = 0
 
+for thing in things {
+    if let value = thing.value as? Double {
+        total += value
+    }
+    else if let value = thing.value as? Int {
+        total += Double(value)
+    }
+}
 //:  Create a variable `total2` of type `Double` set to 0. Loop through the collection again, adding up all the integers and doubles. For each string that you come across during the loop, attempt to convert the string into a number, and add that value to the total. Ignore booleans. Print the total.
  
 
