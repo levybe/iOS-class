1eb35b23a583aecc266e7677f6114a912bf84b44
Benjamin Levy <levybe@ssfs.org>
2022-04-07 14:29:25 -0400

    Finished page 1 and part of page 2.

--- 3 - Navigation and Workflows/4 - Scope/Lab - Scope.playground/Pages/1. Exercise - Scope.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/4 - Scope/Lab - Scope.playground/Pages/1. Exercise - Scope.xcplaygroundpage/Contents.swift
@@ -8,6 +8,7 @@ for _ in 0..<10 {
     print("The value of foo is \(foo)")
 }
 //print("The value of foo is \(foo)")
+print("Because the constant \"foo\" is only defined inside of the for loop.")
 
 
 //:  Using a comment or print statement, describe why both print statements below compile when similar-looking code did not compile above. In what scope is `x` defined, and in what scope is it modified? In contrast, in what scope is `foo` defined and used?
@@ -17,16 +18,32 @@ for _ in 0..<10 {
     print("The value of x is \(x)")
 }
 print("The final value of x is \(x)")
+print("Because x was defined outside of the for loop. In the entire code, x is defined. In the for loop, x is modified. foo is only defined in the for loop, and is used in the for loop as well. Trying to use the constant outside of the for loop wouldn't work.")
 
 
 //:  In the body of the function `greeting` below, use variable shadowing when unwrapping `greeting`. If `greeting` is successfully unwrapped, print a statement that uses the given greeting to greet the given name (i.e. if `greeting` successfully unwraps to have the value "Hi there" and `name` is `Sara`, print "Hi there, Sara."). Otherwise, use "Hello" to print a statement greeting the given name. Call the function twice, once passing in a value for greeting, and once passing in `nil`.
 func greeting(greeting: String?, name: String) {
-
+    if let greeting = greeting {
+        print("\(greeting), \(name).")
+    }
+    else {
+        print("Hello, \(name).")
+    }
 }
-
+greeting(greeting: "What's up", name: "Sarah")
 
 //:  Create a class called `Car`. It should have properties for `make`, `model`, and `year` that are of type `String`, `String`, and `Int`, respectively. Since this is a class, you'll need to write your own memberwise initializer. Use shadowing when naming parameters in your initializer.
-
+class Car {
+    var make: String
+    var model: String
+    var year: Int
+    
+    init(make: String, model: String, year: Int) {
+        self.make = make
+        self.model = model
+        self.year = year
+    }
+}
 
 /*:
 page 1 of 2  |  [Next: App Exercise - Step Competition](@next)
