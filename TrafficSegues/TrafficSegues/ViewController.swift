6017d31552b5f965471e2347129e02cb5f135b33
Benjamin Levy <levybe@ssfs.org>
2022-04-14 09:55:32 -0400

    Worked on Segue practice.

---
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Benjamin Ethan Levy on 4/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segueSwitch: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "yellow", sender: nil)
+    }
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "green", sender: nil)
+    }
+    
+}
+
