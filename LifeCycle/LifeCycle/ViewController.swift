5a612b2e8904bbf04c68b01d601784a05e5db8b4
Benjamin Levy <levybe@ssfs.org>
2022-04-19 14:29:15 -0400

    Started implementing viewController code.

---
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Benjamin Ethan Levy on 4/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ViewController - View Did Load")
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+
+}
+
