2f242f7d3e53c72be1c82581dc23195720f8d360
Benjamin Levy <levybe@ssfs.org>
2022-04-29 15:14:37 -0400

    Worked on PersonalityQuiz up to making the first quiz question appear when the app is run.

--- PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -9,14 +9,28 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
     @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
     
     
     @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
     
     
     @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangeLabel1: UILabel!
+    @IBOutlet weak var rangeLabel2: UILabel!
     
+    @IBOutlet weak var questionProgressView: UIProgressView!
     
     var questions: [Question] = [
       Question(
@@ -61,23 +75,49 @@ class QuestionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[3].text
+    }
+    
+    func updateRangedStack(using answers: [Answer]) {
+        rangedStackView.isHidden = false
+        rangeLabel1.text = answers.first?.text
+        rangeLabel2.text = answers.last?.text
+    }
+    
     func updateUI() {
         singleStackView.isHidden = true
-            multipleStackView.isHidden = true
-            rangedStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
 
-            navigationItem.title = "Question #\(questionIndex + 1)"
+        let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        let totalProgress = Float(questionIndex) / Float(questions.count)
 
-            let currentQuestion = questions[questionIndex]
+        navigationItem.title = "Question #\(questionIndex+1)"
+        questionLabel.text = currentQuestion.text
+        questionProgressView.setProgress(totalProgress, animated: true)
 
-            switch currentQuestion.type {
-            case .single:
-                singleStackView.isHidden = false
-            case .multiple:
-                multipleStackView.isHidden = false
-            case .ranged:
-                rangedStackView.isHidden = false
-            }
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack(using: currentAnswers)
+        case .multiple:
+            updateMultipleStack(using: currentAnswers)
+        case .ranged:
+            updateRangedStack(using: currentAnswers)
+        }
     }
     
 
