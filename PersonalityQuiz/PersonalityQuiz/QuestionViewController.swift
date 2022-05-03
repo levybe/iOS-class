1388bcf219e8d572f39024760412906cd6aaf1b2
Benjamin Levy <levybe@ssfs.org>
2022-05-03 08:59:01 -0400

    Worked on PersonalityQuiz up to adding code to the ResultsViewController.

--- PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -24,13 +24,43 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multiLabel2: UILabel!
     @IBOutlet weak var multiLabel3: UILabel!
     @IBOutlet weak var multiLabel4: UILabel!
-    
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     
     @IBOutlet weak var rangedStackView: UIStackView!
     @IBOutlet weak var rangeLabel1: UILabel!
     @IBOutlet weak var rangeLabel2: UILabel!
+    @IBOutlet weak var rangedSlider: UISlider!
     
     @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[3].text
+    }
+    
+    func updateRangedStack(using answers: [Answer]) {
+        rangedStackView.isHidden = false
+        rangeLabel1.text = answers.first?.text
+        rangeLabel2.text = answers.last?.text
+    }
+    
+    func nextQuestion() {
+        
+    }
     
     var questions: [Question] = [
       Question(
@@ -68,6 +98,7 @@ class QuestionViewController: UIViewController {
     ]
     
     var questionIndex = 0
+    var answersChosen: [Answer] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,27 +106,52 @@ class QuestionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func updateSingleStack(using answers: [Answer]) {
-        singleStackView.isHidden = false
-        singleButton1.setTitle(answers[0].text, for: .normal)
-        singleButton2.setTitle(answers[1].text, for: .normal)
-        singleButton3.setTitle(answers[2].text, for: .normal)
-        singleButton4.setTitle(answers[3].text, for: .normal)
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+
+            switch sender {
+            case singleButton1:
+                answersChosen.append(currentAnswers[0])
+            case singleButton2:
+                answersChosen.append(currentAnswers[1])
+            case singleButton3:
+                answersChosen.append(currentAnswers[2])
+            case singleButton4:
+                answersChosen.append(currentAnswers[3])
+            default:
+                break
+            }
+
+            nextQuestion()
     }
     
-    func updateMultipleStack(using answers: [Answer]) {
-        multipleStackView.isHidden = false
-        multiLabel1.text = answers[0].text
-        multiLabel2.text = answers[1].text
-        multiLabel3.text = answers[2].text
-        multiLabel4.text = answers[3].text
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+
+            if multiSwitch1.isOn {
+                answersChosen.append(currentAnswers[0])
+            }
+            if multiSwitch2.isOn {
+                answersChosen.append(currentAnswers[1])
+            }
+            if multiSwitch3.isOn {
+                answersChosen.append(currentAnswers[2])
+            }
+            if multiSwitch4.isOn {
+                answersChosen.append(currentAnswers[3])
+            }
+
+            nextQuestion()
     }
     
-    func updateRangedStack(using answers: [Answer]) {
-        rangedStackView.isHidden = false
-        rangeLabel1.text = answers.first?.text
-        rangeLabel2.text = answers.last?.text
-    }
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+            let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+
+            answersChosen.append(currentAnswers[index])
+
+            nextQuestion()
+        }
     
     func updateUI() {
         singleStackView.isHidden = true
--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    var responses: [Answer]
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
